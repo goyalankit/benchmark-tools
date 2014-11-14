18bf60a1e1c48f2418e729d76e1899ded7036b99
Ankit Goyal <ankit3goyal@gmail.com>
2014-11-14 11:30:17 -0600

    adding code to get random numbers using gsl

---
+++ sequence_generator/gsl_rng_mt19937.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+gsl_rng *gBaseRand;       /* global rand number generator */
+
+int main (void) {
+    unsigned long randSeed;
+    int i, k, n = 10;
+    double lambda = 3.0;
+
+    /* specifying to use Mersenne twister MT-19937 as the uniform PRNG */
+    gBaseRand = gsl_rng_alloc(gsl_rng_mt19937);
+
+    srand(time(NULL));                    /* initialization for rand() */
+    randSeed = rand();                    /* returns a non-negative integer */
+    gsl_rng_set (gBaseRand, randSeed);    /* seed the PRNG */
+
+    /* print n random variates chosen from  the poisson distribution with mean 
+       parameter lambda */
+    for (i = 0; i < n; i++) {
+        k = gsl_ran_poisson (gBaseRand, lambda);
+        printf (" %u", k);
+    }
+
+    printf ("\n");
+    gsl_rng_free(gBaseRand);
+    return 0;
+}
